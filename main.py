a8fc86ea851e40b087687b1fc992fc03a913b24e
Leandro <leandro.rawicz@coinfabrik.com>
2019-05-07 18:46:32 -0300

    clean

--- main.py
+++ main.py
@@ -16,19 +16,8 @@ ALLin=Pool02 + Pool03
 
 with open('config.json') as json_file:  
     data = json.load(json_file)
-#    for p in data['config']:
-#		code = p['code']
- #       print('code: ' + code)
- #       csv = "DB/CSV/" + p['csv']
- #       print("csv: " +  csv)
- #       jugadores =  p['jugadores']
- ##       letraInicial = p['letraInicial']
- #       drafteos = p['drafteos']
- #       print('boosters: ' + p['boosters'])
- #       print('')
 poolUsado = data["pools"][data["poolUsado"]]
 poolUsado = poolUsado.replace(" ","")
-
 csv = "DB/CSV/" + data["csv"]
 code = data["code"]
 letter = data["letraInicial"]
@@ -37,35 +26,26 @@ boosterDef = data["boosterDef"]
 drafteos = data["drafteos"]
 
 
-
-###### Editar
-##with open('config.json') as data_file:    
-#    data = json.load(data_file)
-#exit()
-
-
-
-
-csv = "DB/CSV/" + "DB Yugi - DB_Draft.csv"
 CodeFolder =  "horno/" + code
-###### 
-
-
-
-
-
 
 fromDir = CodeFolder + "/pool/"
 toDir =  CodeFolder + "/seleccion/"
 
 print(poolUsado)
 print("filtrando cartas")
+
+
+
+#1
 GeneratePoolFromCSV.main(csv,"DB/RAW/",CodeFolder,poolUsado)
 
 for x in xrange(0,drafteos):
+#2
 	print("seleccion de cartas:" + letter)
 	makebooster.main(fromDir,toDir, code, letter, cantidadJugadores, boosterDef )
+#3
 	print("Armado de PDF:" + letter)
 	makePDF.main(code,letter)
+#change letter
 	letter = chr(ord(letter) + 1)
 	
